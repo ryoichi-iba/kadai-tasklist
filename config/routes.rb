ffa46b94fa17359291193ae9638456d9812874ea
ryoichi.iba <ryoichiaz18@gmail.com>
2019-03-18 06:06:26 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+  
+  get 'signup', to: 'users#new'
+
   root to: 'tasks#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   resources :tasks
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  resources :users, only: [:index, :show, :new, :create]
+  end
